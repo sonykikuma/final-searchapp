e951be27a223e7db680731b08c62a54cdf8e518e
abha <abha@example.com>
2022-08-19 20:46:21 +0530

    first commit

---
+++ redditapi.js
@@ -0,0 +1,12 @@
+export default {
+  search: function (searchTerm, searchLimit, sortBy) {
+    // console.log("search....");
+    // fetch("http://www.reddit.com/r/pics/.json?jsonp=${searchTerm}")
+    return fetch(
+      `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+    )
+      .then((res) => res.json())
+      .then((data) => data.data.children.map((data) => data.data))
+      .catch((err) => console.log(err));
+  },
+};
